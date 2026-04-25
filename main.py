cbf9d6acb9eb681f7d2c9f79f998f6efb0a1dd72
ramji4545w-prog <ramji4545w@gmail.com>
2026-04-25 13:35:32 +0530

    perf: WAL mode + threaded Flask + cache

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ from admin import app as flask_app
 
 def run_flask():
     port = int(os.environ.get("PORT", 5000))
-    flask_app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    flask_app.run(host="0.0.0.0", port=port, debug=False,
+                  use_reloader=False, threaded=True)
 
 t = threading.Thread(target=run_flask, daemon=True)
 t.start()
